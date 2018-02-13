16cb72c546cbb9e0a7ea58fe1a448b3ef0be6ebe
Rhys Gale <rhys.gale@hotmail.co.uk>
2018-02-13 14:09:57 +0000

    Training Data Classes Created
    Implemented classes which keep track of the training data we will use to train the neural network.

---
+++ FlappyBirdNeuralNetwork/FlappyBirdNeuralNetwork/NeuralNetwork/DataPiece.cs
@@ -0,0 +1,9 @@
+﻿namespace FlappyBirdNeuralNetwork.NeuralNetwork
+{
+    internal class DataPiece
+    {
+        public int _InputA;
+        public int _InputB;
+        public int _Output;
+    }
+}
---
+++ FlappyBirdNeuralNetwork/FlappyBirdNeuralNetwork/NeuralNetwork/TrainingData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBirdNeuralNetwork.NeuralNetwork
+{
+    //Class used for saving training data
+    internal class TrainingData
+    {
+        private List<DataPiece> _TrainingData;
+
+        internal TrainingData()
+        {
+            _TrainingData = new List<DataPiece>();
+        }
+
+        internal void CreateTrainingData(int in1, int in2, int op)
+        {
+            _TrainingData.Add(new DataPiece
+            {
+                _InputA = in1,
+                _InputB = in2,
+                _Output = op
+            });
+        }
+
+        internal List<DataPiece> GetTrainingData()
+        {
+            return _TrainingData;
+        }
+    }
+}
